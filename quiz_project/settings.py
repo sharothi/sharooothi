5dc3a2b75fc1905c5ea7ecedf7a04ebe7792242f
sharothi <47345726+sharothi@users.noreply.github.com>
2024-09-24 10:12:23 +0600

    Update settings.py
    remove email config

--- quiz_project/settings.py
+++ quiz_project/settings.py
@@ -152,9 +152,4 @@ LOGIN_URL = 'login'
 
 # @@@@@=====================email =================@@@@@
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'somadh'
-EMAIL_HOST_PASSWORD = 
+
